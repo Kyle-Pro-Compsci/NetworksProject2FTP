6ee5dfc6d45ce9d6c46de677148c3c970ed1f991
Kyle Koo <kylekoo837@gmail.com>
2022-09-30 00:09:57 -0400

    data transfer working

--- test.py
+++ test.py
@@ -40,7 +40,43 @@ def byte_test():
     decoded = encoded.decode()
     print(decoded)
     print('\r\n' in decoded)
-    
+
+def partition_test():
+    string = "227 Entering Passive Mode (54,88,34,75,4,188)."
+    after_partition = string.partition("(")
+    print(type(after_partition))
+    print(after_partition)
+    nums = after_partition[2]
+    print(nums.partition(")")[0])
+
+    one_move = string.partition('(')[2].partition(')')[0].split(',')
+    print(one_move)
+    print(type(one_move))
+    ip = one_move[0] + '.' + one_move[1] + '.' + one_move[2] + '.' + one_move[3]
+    port = int(one_move[4]) * pow(2, 8) + int(one_move[5])
+    print(ip)
+    print(type(port), port)
+    print(195 * pow(2, 8) + 149)
+
+    lhs = '{0:08b}'.format(int(one_move[4]))
+    rhs = '{0:08b}'.format(int(one_move[5]))
+    print(lhs)
+    print(rhs)
+    print(int(lhs + rhs, 2))
+
+    lhs_test = '{0:08b}'.format(195)
+    rhs_test = '{0:08b}'.format(149)
+    print(lhs_test)
+
+    print(int(lhs_test+rhs_test, 2))
+
+def url_parse_test_2():
+    test1 = "ftp://bob:s3cr3t@ftp.example.com/"
+    test2 = "ftp://bob:s3cr3t@ftp.example.com/documents/homeworks/homework1.docx"
+    test3 = "other-hw/essay.pdf"
+
+    u = urlparse(test3)
+    print(u.path)
 
 if __name__ == '__main__':
-    byte_test()
+    url_parse_test_2()
